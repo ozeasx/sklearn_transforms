5e8f27b43c7b570ba59e9f790176e90a72a0df2c
Ozéas Carvalho <ozeasx@gmail.com>
2020-08-22 05:53:19 -0300

    try to remove column only if present

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,16 +1,14 @@
-from sklearn.base import BaseEstimator, TransformerMixin
-
-
 # All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns(BaseEstimator, TransformerMixin):
+class DropColumns:
     def __init__(self, columns):
-        self.columns = columns
+        self._columns = columns
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        for c in self._columns:
+            if c in data:
+                data.drop(labels=self._columns, axis='columns', inplace=True)
+        return data
